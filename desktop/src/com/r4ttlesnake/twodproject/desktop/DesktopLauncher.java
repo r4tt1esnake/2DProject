be4f3518b01f524b7225496a26435a8f1cae3393
R4tt1esnake <r4tt1esnake@users.noreply.github.com>
2018-02-23 11:15:01 -0800

    FIXED

--- desktop/src/com/r4ttlesnake/twodproject/desktop/DesktopLauncher.java
+++ desktop/src/com/r4ttlesnake/twodproject/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package com.r4ttlesnake.twodproject.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.r4ttlesnake.twodproject.Game;
+import com.r4ttlesnake.twodproject.game;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Game(), config);
+		new LwjglApplication(new game(), config);
 	}
 }
