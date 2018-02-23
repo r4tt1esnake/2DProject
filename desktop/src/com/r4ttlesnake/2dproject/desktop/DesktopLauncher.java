13e85223879cf86868e153d1cc8bc55a0cac64e8
R4tt1esnake <r4tt1esnake@users.noreply.github.com>
2018-02-23 11:09:45 -0800

    Fixed package issues

--- desktop/src/com/r4ttlesnake/2dproject/desktop/DesktopLauncher.java
+++ desktop/src/com/r4ttlesnake/2dproject/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package com.r4ttlesnake.2dproject.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.r4ttlesnake.2dproject.2DProject;
+import com.r4ttlesnake.2dproject.Game;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new 2DProject(), config);
+		new LwjglApplication(new Game(), config);
 	}
 }
