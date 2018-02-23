47ecc31f02e61fbcb6a838794657c72df03aef69
R4tt1esnake <r4tt1esnake@users.noreply.github.com>
2018-02-23 11:05:32 -0800

    Generated LibGDX Files

---
+++ desktop/src/com/r4ttlesnake/2dproject/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.r4ttlesnake.2dproject.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.r4ttlesnake.2dproject.2DProject;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new 2DProject(), config);
+	}
+}
